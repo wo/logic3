90d4116b17f2aa906e52cb6da508281cab4608d6
wo <wo@umsu.de>
2025-09-19 20:25:33 +0100

    up to 4

--- figures/.auctex-auto/zigzag.el
+++ figures/.auctex-auto/zigzag.el
@@ -3,15 +3,6 @@
 (TeX-add-style-hook
  "zigzag"
  (lambda ()
-   (TeX-add-to-alist 'LaTeX-provided-class-options
-                     '(("standalone" "tikz" "border=5pt")))
-   (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("tikz" "")))
-   (TeX-run-style-hooks
-    "latex2e"
-    "standalone"
-    "standalone10"
-    "tikz")
    (TeX-add-symbols
     "N"
     "NN")
